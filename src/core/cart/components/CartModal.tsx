4e16242624ee1fd5ab9064cfcba3caa6ffbffd31
José Núñez <jose.nunez.rv@gmail.com>
2022-01-29 23:41:20 -0300

    Refactor

---
+++ src/core/cart/components/CartModal.tsx
@@ -0,0 +1,38 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+} from "@chakra-ui/react";
+
+import { useCart } from "core/cart/hooks";
+
+const Cart = ({ onBuy }: any) => {
+  const { isOpen, toggleOpen } = useCart();
+
+  return (
+    <Modal isOpen={isOpen} onClose={toggleOpen}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>My cart</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>...</ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={onBuy}>
+            Comprar
+          </Button>
+          <Button variant="ghost" onClick={toggleOpen}>
+            Cerrar
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default Cart;
