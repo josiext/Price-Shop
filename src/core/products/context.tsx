2d2447ae76a7f2bce0a20f47e79c6d2ee6cbf7b1
José Núñez <jose.nunez.rv@gmail.com>
2022-01-29 22:58:03 -0300

    Refactor

--- src/core/products/context.tsx
+++ src/core/products/context.tsx
@@ -2,20 +2,25 @@ import { createContext, useState } from "react";
 
 import { Product } from "core/products/types";
 
-const defaultValues = {
+interface State {
+  isOpen: boolean;
+  products: Product[];
+}
+
+const defaultValues: State = {
   isOpen: false,
-  products: [] as Product[],
+  products: [],
 };
 
 export const CartContext = createContext([defaultValues, () => {}] as [
-  typeof defaultValues,
-  (d: typeof defaultValues) => void
+  State,
+  (d: State) => void
 ]);
 
 export const CartContextProvider = (props: { children: React.ReactNode }) => {
-  const [state, setState] = useState({
+  const [state, setState] = useState<State>({
     isOpen: false,
-    products: [] as Product[],
+    products: [],
   });
 
   return (
