0eb3e6002b6b3eeff39276ce78813fab31feac2b
José Núñez <79015124+josiext@users.noreply.github.com>
2022-03-06 12:52:38 -0300

    Implementar prisma

--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,3 @@
-import { Sequelize } from "sequelize";
+const x = "shomething";
 
-const DB_URI = "postgres://postgres:1234@localhost:5432/priceshop";
-
-let DatabaseConn: Sequelize | undefined;
-
-const getDB = (): Sequelize => {
-  if (!DatabaseConn) {
-    DatabaseConn = new Sequelize(DB_URI);
-  }
-  return DatabaseConn;
-};
-
-export { getDB };
+export default x;
--- src/pages/api/product.ts
+++ src/pages/api/product.ts
@@ -1,35 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { Sequelize, DataTypes } from "sequelize";
 
-import { Product } from "core/products/types";
-import { getDB } from "database";
-
-const DatabaseConn = getDB();
-
-const User = DatabaseConn.define(
-  "User",
-  {
-    // Model attributes are defined here
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      // allowNull defaults to true
-    },
-  },
-  {
-    // Other model options go here
-  }
-);
-
-// `sequelize.define` also returns the model
-console.log(User === DatabaseConn.models.User); // true
-
-const PRODUCTS: Product = {
+const PRODUCTS: any = {
   id: "2",
-  title: "Product 1 Product 1 Product 1 Product 1 Product 1 Product 1 ",
+  title: "Product 1",
   price: 150,
   images: ["https://bit.ly/2Z4KKcF"],
   category: {
@@ -40,10 +13,8 @@ const PRODUCTS: Product = {
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Product>
+  res: NextApiResponse<typeof PRODUCTS>
 ) {
-  console.log(await User.findAll());
-
   if (req.method === "GET") return res.status(200).json(PRODUCTS);
   if (req.method === "POST") return res.status(200).json(PRODUCTS);
   if (req.method === "DELETE") return res.status(200).json(PRODUCTS);
